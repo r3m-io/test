14f4b1524f6ac4d6478b4a49802b3dcd408c483e
remco van der Velde <remco.pc@outlook.com>
2024-05-24 18:29:03 +0200

    adding installed modules

--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -31,7 +31,7 @@ trait Main {
             ]);
             throw $exception;
         }
-        Core::execute($object, 'composer show -i', $output, $notification);
+        Core::execute($object, 'composer show', $output, $notification);
         if($output){
             echo $output;
         }
